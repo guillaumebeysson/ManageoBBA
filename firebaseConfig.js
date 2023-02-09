b223755747be77b8b82536d8e4cf140c5dfb20da
Beysson Guillaume <guillaume.beysson@gmail.com>
2023-02-09 09:14:02 +0100

    pull ordi portable

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,28 +7,28 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebas
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBkid7lN5W3T52Hfe7rzaNizom_-PVUvsg",
-    authDomain: "fir-9cb64.firebaseapp.com",
-    databaseURL:
-        "https://fir-9cb64-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "fir-9cb64",
-    storageBucket: "fir-9cb64.appspot.com",
-    messagingSenderId: "938406475697",
-    appId: "1:938406475697:web:0a3b420234856e36b66f90",
-    measurementId: "G-GKKLV6XWPS",
+  apiKey: "AIzaSyBkid7lN5W3T52Hfe7rzaNizom_-PVUvsg",
+  authDomain: "fir-9cb64.firebaseapp.com",
+  databaseURL:
+    "https://fir-9cb64-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-9cb64",
+  storageBucket: "fir-9cb64.appspot.com",
+  messagingSenderId: "938406475697",
+  appId: "1:938406475697:web:0a3b420234856e36b66f90",
+  measurementId: "G-GKKLV6XWPS",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 import {
-    getDatabase,
-    ref,
-    set,
-    get,
-    child,
-    update,
-    remove,
+  getDatabase,
+  ref,
+  set,
+  get,
+  child,
+  update,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
 
 const db = getDatabase();
@@ -39,18 +39,18 @@ var menu = document.getElementById("menu");
 
 // Push en base
 function save() {
-    set(ref(db, username.value.toUpperCase()), {
-        username: username.value,
-        menu: menu.value,
-        date: Date.now(),
+  set(ref(db, username.value.toUpperCase()), {
+    username: username.value,
+    menu: menu.value,
+    date: Date.now(),
+  })
+    .then(() => {
+      alert("commande envoyée");
+      console.log("commande envoyée");
     })
-        .then(() => {
-            alert("commande envoyée");
-            console.log("commande envoyée")
-        })
-        .catch((error) => {
-            alert("erreur " + error);
-        });
+    .catch((error) => {
+      alert("erreur " + error);
+    });
 }
 
 // function selectData() {
@@ -78,31 +78,31 @@ function save() {
 //             alert("erreur " + error);
 //         });
 // }
+//
 
 function selectData() {
-    const dbref = ref(db);
-    get(dbref, username.value)
-        .then((snapshot) => {
-            if (snapshot.exists()) {
-                username.value = snapshot.val().username;
-                // console.log(snapshot.val());
-                var data = snapshot.val();
-                for (let i in data) {
-                    console.log(data[i])
-                }
-                document.getElementById("commande").innerHTML =
-                    snapshot.val().toUpperCase();
-                menu.value = snapshot.val().menu;
-            } else {
-                alert("No data found");
-            }
-        })
-        .catch((error) => {
-            alert("erreur " + error);
-        });
+  const dbref = ref(db);
+  get(dbref, username.value)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        username.value = snapshot.val().username;
+        // console.log(snapshot.val());
+        var data = snapshot.val();
+        for (let i in data) {
+          console.log(data[i]);
+        }
+        document.getElementById("commande").innerHTML = snapshot
+          .val()
+          .toUpperCase();
+        menu.value = snapshot.val().menu;
+      } else {
+        alert("No data found");
+      }
+    })
+    .catch((error) => {
+      alert("erreur " + error);
+    });
 }
-
-
 
 valid.addEventListener("click", save);
 voirCommande.addEventListener("click", selectData);
