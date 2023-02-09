b7a127d6543224442d677b22f0700afdef46ad44
guillaumebeysson <guillaume.beysson@gmail.com>
2023-02-09 23:20:00 +0100

    voir commande toggle

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,28 +7,28 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebas
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkid7lN5W3T52Hfe7rzaNizom_-PVUvsg",
-  authDomain: "fir-9cb64.firebaseapp.com",
-  databaseURL:
-    "https://fir-9cb64-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fir-9cb64",
-  storageBucket: "fir-9cb64.appspot.com",
-  messagingSenderId: "938406475697",
-  appId: "1:938406475697:web:0a3b420234856e36b66f90",
-  measurementId: "G-GKKLV6XWPS",
+    apiKey: "AIzaSyBkid7lN5W3T52Hfe7rzaNizom_-PVUvsg",
+    authDomain: "fir-9cb64.firebaseapp.com",
+    databaseURL:
+        "https://fir-9cb64-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fir-9cb64",
+    storageBucket: "fir-9cb64.appspot.com",
+    messagingSenderId: "938406475697",
+    appId: "1:938406475697:web:0a3b420234856e36b66f90",
+    measurementId: "G-GKKLV6XWPS",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 import {
-  getDatabase,
-  ref,
-  set,
-  get,
-  child,
-  update,
-  remove,
+    getDatabase,
+    ref,
+    set,
+    get,
+    child,
+    update,
+    remove,
 } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
 
 const db = getDatabase();
@@ -36,93 +36,79 @@ const db = getDatabase();
 var username = document.getElementById("username");
 var valid = document.getElementById("valid");
 var menu = document.getElementById("menu");
-var masquer = document.getElementById("masquerCommande");
+var commandes = document.getElementById("commande");
+
 
 // Push en base
 function save() {
-  set(ref(db, username.value.toUpperCase()), {
-    username: username.value,
-    menu: menu.value,
-    date: Date.now(),
-  })
-    .then(() => {
-      alert("commande envoyée");
-      console.log("commande envoyée");
+    set(ref(db, username.value.toUpperCase()), {
+        username: username.value,
+        menu: menu.value,
+        date: Date.now(),
     })
-    .catch((error) => {
-      alert("erreur " + error);
-    });
+        .then(() => {
+            alert("commande envoyée");
+            console.log("commande envoyée");
+        })
+        .catch((error) => {
+            alert("erreur " + error);
+        });
+    commandes.style.display = "none";
 }
 
-// function selectData() {
-//     db.ref.once(username.value, function (snapshot) {
-//         console.log(snapshot.val());
-//     })
-// }
-
-// read data
-// function selectData() {
-//     const dbref = ref(db);
-//     get(child(dbref, username.value))
-//         .then((snapshot) => {
-//             if (snapshot.exists()) {
-//                 username.value = snapshot.val().username;
-//                 console.log(snapshot.val());
-//                 menu.value = snapshot.val().menu;
-//                 document.getElementById("commande").innerHTML =
-//                     snapshot.val().username.toUpperCase() + ": " + snapshot.val().menu;
-//             } else {
-//                 alert("No data found");
-//             }
-//         })
-//         .catch((error) => {
-//             alert("erreur " + error);
-//         });
-// }
-//
 
 function selectData() {
-  const dbref = ref(db);
-  get(dbref, username.value)
-    .then((snapshot) => {
-      var data = snapshot.val();
-      document.getElementById("commande").innerHTML = "";
-      if (snapshot.exists()) {
-        for (let i in data) {
-          console.log(data[i]);
-          // transforme la date sous forme de timestamp en DD/MM/YYYY
-          let timestamp = data[i].date;
-          let date = new Date(timestamp);
-          let options = {
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric",
-          };
-          let formattedDate = date.toLocaleDateString("fr-FR", options);
-          // récupère le nom
-          let name = data[i].username;
-          // récupère le menu
-          let menu = data[i].menu;
-          // push la date, le nom et le menu en html
-          document.getElementById("commande").innerHTML +=
-            formattedDate + ": " + name.toUpperCase() + " --> " + menu + "<br>";
-          // document.getElementById("masquerCommande").innerHTML =
-          //   "<button id='btnMasquerCommande' class='validBtn'>Masquer les commandes</button>";
-        }
-      } else {
-        alert("No data found");
-      }
-    })
-    .catch((error) => {
-      alert("erreur " + error);
-    });
+    // toggle pour afficher ou non les commandes
+    if (commandes.style.display === "block") {
+        commandes.style.display = "none";
+    } else {
+        commandes.style.display = "block";
+    }
+    // récupère les données de firebase
+    const dbref = ref(db);
+    get(dbref, username.value)
+        .then((snapshot) => {
+            var data = snapshot.val();
+            document.getElementById("commande").innerHTML = "";
+            if (snapshot.exists()) {
+                for (let i in data) {
+                    console.log(data[i]);
+                    // transforme la date sous forme de timestamp en DD/MM/YYYY
+                    let timestamp = data[i].date;
+                    let date = new Date(timestamp);
+                    let options = {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                    };
+                    let formattedDate = date.toLocaleDateString("fr-FR", options);
+                    // récupère le nom
+                    let name = data[i].username;
+                    // récupère le menu
+                    let menu = data[i].menu;
+                    // push la date, le nom et le menu en html
+                    document.getElementById("commande").innerHTML +=
+                        "<li>"
+                        +
+                        "<p class='dateCommande'>" + formattedDate + "</p>"
+                        +
+                        "<p class='usernameCommande'>" + name.toUpperCase() + "</p>"
+                        +
+                        "<p class='menuCommande'>" + " --> " + menu + "</p>"
+                        +
+                        "</li>";
+
+                }
+            } else {
+                alert("No data found");
+            }
+        })
+        .catch((error) => {
+            alert("erreur " + error);
+        });
 }
 
-// function masquerCommande() {
-//   document.getElementById("btnMasquerCommande").classList.add("hidden");
-//   document.getElementById("commande").classList.add("hidden");
-// }
 
-// masquer.addEventListener("click", masquerCommande);
+
 valid.addEventListener("click", save);
 voirCommande.addEventListener("click", selectData);
